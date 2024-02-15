e824046b5dc42d515ef4b8a4e4fb84c4df15c412
Joachim Breitner <mail@joachim-breitner.de>
2024-02-15 15:59:01 +0100

    Towards an ignorelist

--- Simplc.lean
+++ Simplc.lean
@@ -2,6 +2,7 @@ import Lean
 import Std.Data.List.Lemmas
 import Std.Lean.Meta.Basic
 import Std.Lean.Delaborator
+import Std.Lean.HashSet
 
 open Lean Meta
 
@@ -51,7 +52,9 @@ def mvarsToContext {α} (es : Array Expr) (e : Expr) (k : Expr → MetaM α) : M
 
 open Elab Tactic
 partial
-def checkSimpLC (thm1 : SimpTheorem) (thms : SimpTheorems): MetaM Unit := withConfig (fun c => { c with etaStruct := .none }) do
+def checkSimpLC (thm1 : SimpTheorem) (thms : SimpTheorems) (ignores : HashSet (Name × Name) := {})
+    : MetaM Unit := withConfig (fun c => { c with etaStruct := .none }) do
+
   -- logInfo m!"Checking {thm1} and {thm2} for critical pairs"
 
   let val1 ← thm1.getValue
@@ -76,6 +79,7 @@ def checkSimpLC (thm1 : SimpTheorem) (thms : SimpTheorems): MetaM Unit := withCo
       -- logInfo m!"Discr: {thms.post}\nt: {t}"
       let matchs := (← thms.pre.getUnify t simpDtConfig) ++ (← thms.post.getUnify t simpDtConfig)
       let matchs := matchs.filter fun thm2 => ! thms.erased.contains thm2.origin
+      let matchs := matchs.filter fun thm2 => ! ignores.contains (thm1.origin.key, thm2.origin.key)
       -- logInfo m!"Matches: {matchs}"
       for thm2 in matchs do
         let val2  ← thm2.getValue
@@ -102,7 +106,7 @@ def checkSimpLC (thm1 : SimpTheorem) (thms : SimpTheorems): MetaM Unit := withCo
             let e2 ← instantiateMVars e2
 
             let msg :=
-              m!"The rules {← My.ppOrigin thm1.origin} and {← My.ppOrigin thm2.origin} produce a critical pair. Expression{indentExpr cp}\n" ++
+              m!"The rules\n    {← My.ppOrigin thm1.origin} {← My.ppOrigin thm2.origin}\nproduce a critical pair. Expression{indentExpr cp}\n" ++
               m!"reduces to{indentExpr e1}\n" ++
               m!"as well as{indentExpr e2}"
 
@@ -160,10 +164,11 @@ def mkSimpTheorem (name : Name) : MetaM SimpTheorem := do
 
 -- Exclude these from checking all
 def lcBlacklist : Array Name := #[
-  ``List.foldrM_append -- causes it to run out of stack space
+  ``List.foldrM_append,    -- causes it to run out of stack space
+  ``List.getElem?_eq_get?  -- oddness with .refl and Decidable
   ]
 
-def checkSimpLCAll : MetaM Unit := do
+def checkSimpLCAll (ignores : HashSet (Name × Name) := {}): MetaM Unit := do
   let sthms ← getSimpTheorems
   let thms := sthms.pre.values ++ sthms.post.values
   let thms := thms.filter fun sthm => Id.run do
@@ -182,7 +187,7 @@ def checkSimpLCAll : MetaM Unit := do
   -- let thms := thms[:104] ++ thms[105:]
   for thm1 in thms do
     try
-      checkSimpLC thm1 filtered_sthms
+      checkSimpLC thm1 filtered_sthms (ignores := ignores)
     catch e => logError m!"Failed to check {← My.ppOrigin thm1.origin}\n{← nestedExceptionToMessageData e}"
 
 open Elab Command in
@@ -191,9 +196,21 @@ elab "check_simp_lc " thm1:ident thm2:ident : command => runTermElabM fun _ => d
   let thm2 ← resolveGlobalConstNoOverload thm2
   checkSimpLC (← mkSimpTheorem thm1) (← mkSimpTheorems thm2)
 
+
+open Parser Term Tactic in
+def ignores : Parser := leading_parser
+  optional ("ignoring " >> sepBy1IndentSemicolon (Parser.ident >> Parser.ident))
+
+
 open Elab Command in
-elab "check_simp_lc" : command => runTermElabM fun _ => do
-  checkSimpLCAll
+elab "check_simp_lc " ign:ignores : command => runTermElabM fun _ => do
+  let ignores ← match ign with
+    | `(ignores|ignoring $[$i1:ident $i2:ident]*) => do
+      let thm1s ← i1.mapM resolveGlobalConstNoOverload
+      let thm2s ← i2.mapM resolveGlobalConstNoOverload
+      pure (HashSet.ofArray (thm1s.zip thm2s))
+    | _ => pure {}
+  checkSimpLCAll ignores
 
 attribute [simp] Function.comp_def
 -- fixes
@@ -215,4 +232,131 @@ attribute [simp] and_assoc
 -- fixes
 -- check_simp_lc List.mem_append List.cons_append
 
-check_simp_lc
+-- Bug: tactic 'rfl' failed, equality expected
+-- check_simp_lc List.getElem?_eq_get? List.getElem?_eq_get?
+
+-- This seems to be needed
+attribute [simp] List.prefix_refl
+attribute [simp] List.suffix_refl
+attribute [simp] List.nil_suffix
+attribute [simp] List.nil_prefix
+attribute [simp] List.nil_infix
+
+
+check_simp_lc ignoring
+  List.disjoint_cons_left List.disjoint_cons_right
+  List.disjoint_cons_left List.disjoint_singleton
+  List.disjoint_cons_left List.disjoint_append_right
+  List.singleton_disjoint List.disjoint_cons_right
+  List.disjoint_append_left List.disjoint_cons_right
+  List.disjoint_append_left List.disjoint_singleton
+  List.disjoint_append_left List.disjoint_append_right
+
+  List.findIdx?_cons List.findIdx?_succ
+
+  List.get?_concat_length List.length_replicate
+  List.get?_concat_length List.length_zipWith
+  List.get?_concat_length List.length_concat
+  List.get?_concat_length List.length_map
+  List.get?_concat_length List.length_set
+  List.get?_concat_length List.length_dropLast
+  List.get?_concat_length List.length_dropLast_cons
+  List.get?_concat_length List.length_tail
+
+  List.reverse_prefix List.reverse_cons
+  List.reverse_prefix List.reverse_reverse
+  List.reverse_prefix List.reverse_append
+  List.reverse_prefix List.reverse_nil
+
+  List.getLast_append List.cons_append
+  List.getLast_append List.singleton_append
+  List.getLast_append List.append_assoc
+  List.getLast_append List.nil_append
+
+  List.filterMap_join List.join_cons
+  List.filterMap_some List.filterMap_join
+
+  List.take_length List.length_replicate
+  List.take_length List.length_zipWith
+  List.take_length List.length_concat
+  List.take_length List.length_map
+  List.take_length List.length_set
+  List.take_length List.length_dropLast
+  List.take_length List.length_dropLast_cons
+  List.take_length List.length_tail
+
+  -- nontrivial precondition.
+  -- Is mem_of_find?_eq_some too particular to be in the default simp set?
+  List.mem_map List.mem_of_find?_eq_some
+  List.mem_reverseAux List.mem_of_find?_eq_some
+  List.mem_cons List.mem_of_find?_eq_some
+  List.mem_singleton List.mem_of_find?_eq_some
+  List.mem_inter_iff List.mem_of_find?_eq_some
+  List.mem_erase_of_ne List.mem_of_find?_eq_some
+  List.mem_reverse List.mem_of_find?_eq_some
+  List.mem_union_iff List.mem_of_find?_eq_some
+  List.mem_filterMap List.mem_of_find?_eq_some
+  List.mem_eraseP_of_neg List.mem_of_find?_eq_some
+  List.mem_append List.mem_of_find?_eq_some
+  List.mem_insert_iff List.mem_of_find?_eq_some
+  List.mem_range'_1 List.mem_of_find?_eq_some
+  List.mem_range List.mem_of_find?_eq_some
+
+  List.mem_map List.map_map
+  List.mem_map List.map_cons
+  List.mem_map List.map_append
+  List.mem_reverseAux List.reverseAux_cons
+  List.mem_reverse List.reverse_cons
+  List.mem_filterMap List.filterMap_join
+  List.mem_filterMap List.filterMap_cons
+  List.mem_eraseP_of_neg List.eraseP_cons_of_pos
+  List.mem_insert_iff List.insert_of_mem
+
+  List.filter_sublist List.filter_cons_of_pos
+  List.filter_sublist List.filter_cons_of_neg
+  List.filter_sublist List.filter_filter
+  List.filter_sublist List.filter_append
+  List.reverse_sublist List.reverse_cons
+  List.reverse_sublist List.reverse_reverse
+  List.reverse_sublist List.reverse_append
+
+  List.append_sublist_append_left List.append_nil
+  List.append_sublist_append_left List.cons_append
+  List.append_sublist_append_left List.singleton_append
+  List.append_sublist_append_left List.take_append_drop
+
+  List.reverse_suffix List.reverse_cons
+  List.reverse_suffix List.reverse_reverse
+  List.reverse_suffix List.reverse_append
+  List.reverse_suffix List.reverse_nil
+
+  List.dropLast_append_cons List.cons_append
+  List.dropLast_concat List.cons_append
+
+  List.enumFrom_map_fst List.enumFrom_cons
+  List.infix_append' List.cons_append
+  List.infix_append' List.singleton_append
+  List.infix_append' List.append_assoc
+  List.infix_append' List.nil_append
+  List.infix_append' List.append_nil
+  List.infix_append' List.take_append_drop
+  List.infix_append' List.range'_append_1
+  List.reverse_infix List.reverse_cons
+  List.reverse_infix List.reverse_reverse
+  List.reverse_infix List.reverse_append
+
+  List.length_take List.take_cons_succ
+  List.length_take List.take_succ_cons
+  List.getLast?_concat List.cons_append
+  List.getLast?_concat List.singleton_append
+  List.getLast?_concat List.append_assoc
+
+  List.sizeOf_get List.get_replicate
+  List.sizeOf_get List.get_map
+  List.sizeOf_get List.get_set_eq
+  List.sizeOf_get List.get_set_ne
+  List.sizeOf_get List.get_dropLast
+  List.sizeOf_get List.get_cons_zero
+  -- List.sizeOf_get List.get_cons_succ' stack overflow
+  List.sizeOf_get List.cons.sizeOf_spec
+  List.append_cancel_left_eq List.append_nil
